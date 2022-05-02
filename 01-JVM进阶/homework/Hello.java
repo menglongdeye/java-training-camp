88f30dfd2b5a61454faf4020558ad1b8c773399d
conglongli <liconglong@163.com>
2022-05-02 11:03:17 +0800

    01-jvm：作业-1.（选做）自己写一个简单的 Hello.java，里面需要涉及基本类型，四则运行，if 和 for，然后自己分析一下对应的字节码

---
+++ 01-JVM进阶/homework/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int max = 10;
+        for (int i=0;i<max;i++){
+            if(i%2 != 0){
+                System.out.println("=====" + i);
+            }
+        }
+    }
+}
